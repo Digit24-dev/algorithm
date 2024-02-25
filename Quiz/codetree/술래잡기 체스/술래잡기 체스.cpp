e227cc78cd377a6f65b1b940540d3d17b6adf5de
CJH <digi7442@gmail.com>
2024-02-25 14:31:39 +0900

    re: 술래잡기 체스

--- Quiz/codetree/술래잡기 체스/술래잡기 체스.cpp
+++ Quiz/codetree/술래잡기 체스/술래잡기 체스.cpp
@@ -1,62 +1,123 @@
 #include <iostream>
 
 #include <vector>
+#include <queue>
+#include <algorithm>
 
 using namespace std;
 
+// 12시부터 반시계방향 정렬
+int di[8] = { -1, -1, 0, 1, 1, 1, 0, -1 };
+int dj[8] = { 0, -1, -1, -1, 0, 1, 1, 1 };
 
-// 12시부터 시계방향 정렬
-int di[9] = {0, -1, -1, 0, 1, 1, 1, 0, -1};
-int dj[9] = {0, 0, 1, 1, 1, 0, -1, -1, -1};
+int answer = 0;
 
-pair<int, int> map[4][4];   // f: 순서, s: dir
+/// <summary>
+/// first: 순서, second: 방향
+/// </summary>
+pair<int, int> map[4][4];
 
-pair<pair<int, int>, int> player;   // cord, dir
+// 스택에 방향과 달라진 맵을 전달
 
-int score = 0;
-
-void input() {
-    int N = 32;
-    int t, i = 0, j = 0;
-
-    while (N)
+int input() {
+    for (int i = 0; i < 4; i++)
     {
-        cin >> t;
-
-        if (N & 0x01) {
-            map[i][j].second = t;
-            ++j;
+        for (int j = 0; j < 4; j++)
+        {
+            cin >> map[i][j].first >> map[i][j].second;
+            map[i][j].second--;
         }
-        else
-            map[i][j].first = t;
+    }
 
-        if (j == 3) {
-            ++i; j = 0;
+    return map[0][0].first;
+}
+
+void moveThief(pair<int, int> _map[4][4])
+{
+    vector<pair<int, int>> dup;
+    dup.reserve(16);
+
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            dup.push_back(_map[i][j]);
         }
-        --N;
+    }
+
+    sort(dup.begin(), dup.end());
+
+    for (int i = 0; i < 16; i++)
+    {
+        if (dup[i].first == 0) continue;
+
+        for (int d = 0; d < 8; d++)
+        {
+            int nd = dup[i].second + d == 7 ? 0 : dup[i].second + d;
+            int ni = (i / 4) + di[nd];
+            int nj = (i % 4) + dj[nd];
+
+            if (ni >= 0 && ni < 4 && nj >= 0 && nj < 4) {
+                if (_map[ni][nj].first != 0) {
+                    swap(_map[ni][nj], _map[i / 4][i % 4]);
+                    break;
+                }
+            }
+        }
     }
 }
 
-void getRubber(int i, int j)
+vector<pair<int, int>> play(pair<int, int> _map[4][4], int player_i, int player_j, int &score)
 {
-    score += map[i][j].first;
-    player.first = make_pair(i, j);
-    player.second = map[i][j].second;
+    vector<pair<int, int>> ret;
+    int ni = player_i, nj = player_j, cnt = 3;
+    while (cnt--)
+    {
+        ni += map[player_i][player_j].second;
+        nj += map[player_i][player_j].second;
+
+        if (ni >= 0 && ni < 4 && nj >= 0 && nj < 4)
+        {
+            if (map[ni][nj].first != 0) {
+                ret.push_back(make_pair(ni, nj));
+                score += _map[ni][nj].first;
+            }
+        }
+    }
+
+    return ret;
 }
 
-pair<int, int> findRub()
+void solution(pair<int,int> _map[4][4], int player_i, int player_j, int score) 
 {
-    int ni = player.first.first + di[player.second];
-    int nj = player.first.second + dj[player.second];
+    // 1. 도둑말 이동
+    pair<int, int> dup[4][4];
+    pair<int, int> dup2[4][4];
+    memcpy(dup, map, sizeof(map));
+    
+    moveThief(dup);
 
-    ;
+    // 2. 술래말 이동
+    vector<pair<int, int>> q = play(dup, player_i, player_j, score);
+
+    memcpy(dup2, dup, sizeof(dup));
+
+    for (auto& i : q)
+    {
+        memcpy(dup, dup2, sizeof(dup2));
+        solution(dup, i.first, i.second, score);
+        answer = max(answer, score);
+    }
 }
 
 int main() {
 
-    input();
-    getRubber(0, 0);
+    int initScore = input();
+    map[0][0].first = 0;
 
+    solution(map, 0, 0, initScore);
+
+    cout << answer << endl;
 
     return 0;
 }
